d17231ac9967521bfa41e14b708aa35e1d0f8b1f
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-14 13:01:05 +0100

    Set up AutoMapper Profile

---
+++ policeDataApi_Practice/Profiles/ApiCallProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace policeDataApi_Practice.Profiles
+{
+    public class ApiCallProfile : Profile
+    {
+        public ApiCallProfile()
+        {
+
+        }
+    }
+}
