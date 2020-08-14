f113bba4205901daa2835e4c2ad4342cc9bbddf5
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-14 13:12:00 +0100

    Add Constructor to new Controller with Interfaces and AutoMapper

---
+++ policeDataApi_Practice/Controllers/StreetCrimesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using policeDataApi_Practice.Data;
+
+namespace policeDataApi_Practice.Controllers
+{
+    [Route("api/streetcrime")]
+    [ApiController]
+    public class StreetCrimesController : Controller
+    {
+        private readonly IStreetLevelCrimesRepo _crimesRepo;
+        private readonly IStreetLevelOutcomesRepo _crimesOutcomesRepo;
+        private readonly IMapper _mapper;
+
+        public StreetCrimesController(IStreetLevelCrimesRepo crimesRepo, IStreetLevelOutcomesRepo crimesOutcomesRepo, IMapper mapper)
+        {
+            _crimesRepo = crimesRepo;
+            _crimesOutcomesRepo = crimesOutcomesRepo;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
