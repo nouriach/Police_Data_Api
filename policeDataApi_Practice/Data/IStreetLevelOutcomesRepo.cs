c467ddf2a7e75fd6938c3e815b15190e2fb3f17c
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-14 11:30:48 +0100

    Start Interfaces and Repos

---
+++ policeDataApi_Practice/Data/IStreetLevelOutcomesRepo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace policeDataApi_Practice.Data
+{
+    public interface IStreetLevelOutcomesRepo
+    {
+        // In here we will need (all GET commands):
+
+        /*
+            1. GetAllStreetLevelOutcomes();
+            2. GetStreetLevelOutcomeByCrimeId();
+
+        MAYBE
+             3. GetStreetLevelOutcomeByCrimeLocation();
+             4. GetStreetLevelOutcomeByCrimeMonth();
+             5. GetStreetLevelOutcomeByResolutionData();
+
+         */
+    }
+}
