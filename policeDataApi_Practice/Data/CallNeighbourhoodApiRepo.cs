fb8813d204bf0d2bb9fb83c8dbf780bd1783415c
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-24 15:46:52 +0100

    Create CallNeighbourhoodApiRepo

---
+++ policeDataApi_Practice/Data/CallNeighbourhoodApiRepo.cs
@@ -0,0 +1,24 @@
+﻿using policeDataApi_Practice.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace policeDataApi_Practice.Data
+{
+    public class CallNeighbourhoodApiRepo : IYourNeighbourhoodRepo
+    {
+        private IHttpClientFactory _clientFactory;
+        public CallNeighbourhoodApiRepo(IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+
+        }
+
+        public Task<DisplayLocalNeighbourhoodViewModel> GetNeighbourhoodLocation()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
