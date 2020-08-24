98b2a2ac275d04f2ef5fe6cb1f9b76e549d19a1a
Nathan O <Nathan.OURIACH@EDUCATION.GOV.UK>
2020-08-24 15:45:17 +0100

    Create interface for Neighbourhood work

--- policeDataApi_Practice/Controllers/YourNeighbourhoodController.cs
+++ policeDataApi_Practice/Controllers/YourNeighbourhoodController.cs
@@ -18,7 +18,19 @@ namespace policeDataApi_Practice.Controllers
         [HttpPost]
         public IActionResult Index(DisplayLocalNeighbourhoodViewModel model)
         {
-            return View();
+
+
+
+            /*
+            var streetLevelResultsByDate = await _crimesRepo.GetAllStreetLevelCrimesByLocationAndTime(model.PostcodePartOne, model.PostcodePartTwo);
+            streetLevelResultsByDate.AllCategories = await _crimesRepo.GetAllCategories();
+            if (streetLevelResultsByDate != null)
+            {
+                return View(streetLevelResultsByDate);
+            }
+
+            return NotFound();
+            */
         }
     }
 }
---
+++ policeDataApi_Practice/Data/IYourNeighbourhoodRepo.cs
@@ -0,0 +1,13 @@
+﻿using policeDataApi_Practice.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace policeDataApi_Practice.Data
+{
+    public interface IYourNeighbourhoodRepo
+    {
+        Task<DisplayLocalNeighbourhoodViewModel> GetNeighbourhoodLocation();
+    }
+}
